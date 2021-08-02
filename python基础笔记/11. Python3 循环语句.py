182e43877085693e5ea7ecebac04f704955918e4
彭财旺 <2787438806@qq.com>
2021-08-02 17:32:54 +0800

    试一试提交功能

---
+++ python基础笔记/11. Python3 循环语句.py
@@ -0,0 +1,35 @@
+# while 循环
+# 同样需要注意冒号和缩进。另外，在 Python 中没有 do..while 循环
+
+# a=1
+# while a<10:
+#     print(a,end=' ')
+#     a+=2
+
+# 用while 计算 1到100的总和
+
+# a,sum=0,0
+# while a<=100:
+#     sum+=a
+#     a+=1
+# print(sum)
+
+# while 循环使用 else 语句
+# 如果 while 后面的条件语句为 false 时，则执行 else 的语句块。
+
+# count = 0
+# while count < 5:
+#    print (count, " 小于 5")
+#    count = count + 1
+# else:
+#    print (count, " 大于或等于 5")
+
+
+# for 语句
+
+# for循环用来遍历
+# languages=["c","c++","perl","python"]
+# for item in languages:
+#     print(item)
+
+
